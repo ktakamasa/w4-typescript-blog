aa622895329b0f169104c9dd5a90d23c2ed5705a
ktakamasa <kohtakamasa@gmail.com>
2023-11-21 18:05:40 +0000

    fixed build errors

--- ts-blog-app/pages/posts/first-post.tsx
+++ ts-blog-app/pages/posts/first-post.tsx
@@ -5,7 +5,7 @@ import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>First Post</title>
       </Head>
